071ed9e376277c5cfa1ac6522cd00ff33f01ce6d
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-01-28 15:05:36 -0500

    attached story for going outside

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ button3 = document.getElementById('button-three');
 UI_text = document.getElementById("UI-text");
 UI_body = document.getElementById("UI-body");
 
-story = { "Home": "You live in a modest house with Ikea furniture and cream colored walls", "Window": "You are infront of a large bay window.", "Window-view": "What a lovely day it is." };
-storyChoice = { "Home": { "button-one": "view window.", "button-two": "view room.", "button-three": "go outside" }, "Window": { "button-one": "look out window", "button-two": "meditate", "button-three": "return to room" } };
+story = { "Home": "You live in a modest house with Ikea furniture and cream colored walls", "Home-view": "You have done well for yourself, but this place seems rather empty", "Window": "You are infront of a large bay window.", "Window-view": "What a lovely day it is.", "Window-meditate":"Ah and so we continue along the path of enlightenment.", "Outside": "What a lovely day. Perhaps, too nice to even go to work?" };
+storyChoice = { "Home": { "button-one": "view window.", "button-two": "view room.", "button-three": "go outside" }, "Window": { "button-one": "look out window", "button-two": "meditate", "button-three": "return to room" }, "Outside": { "button-one": "go to work.", "button-two": "go to the park.", "button-three": "return home."} };
 
 main();
 
@@ -24,17 +24,39 @@ function main() {
             button3.innerHTML = storyChoice["Home"]["button-three"];
             UI_body.style.background = "pink";
             break;
+        case "Home-view": UI_text.innerHTML = story["Home-view"];
+            button1.innerHTML = storyChoice["Home"]["button-one"];
+            button2.innerHTML = storyChoice["Home"]["button-two"];
+            button3.innerHTML = storyChoice["Home"]["button-three"];
+            UI_body.style.background = "pink";
+            state = "Home";
+            break;
         case "Window": UI_text.innerHTML = story["Window"];
             button1.innerHTML = storyChoice["Window"]["button-one"];
             button2.innerHTML = storyChoice["Window"]["button-two"];
             button3.innerHTML = storyChoice["Window"]["button-three"];
             UI_body.style.background = "blue";
             break;
-        case "Window-view" : UI_text.innerHTML = story["Window-view"];
-        button1.innerHTML = storyChoice["Window"]["button-one"];
-        button2.innerHTML = storyChoice["Window"]["button-two"];
-        button3.innerHTML = storyChoice["Window"]["button-three"];
-        UI_body.style.background = "blue";
+        case "Window-view": UI_text.innerHTML = story["Window-view"];
+            button1.innerHTML = storyChoice["Window"]["button-one"];
+            button2.innerHTML = storyChoice["Window"]["button-two"];
+            button3.innerHTML = storyChoice["Window"]["button-three"];
+            UI_body.style.background = "blue";
+            state = "Window";
+            break;
+        case "Window-meditate": UI_text.innerHTML = story["Window-meditate"];
+            button1.innerHTML = storyChoice["Window"]["button-one"];
+            button2.innerHTML = storyChoice["Window"]["button-two"];
+            button3.innerHTML = storyChoice["Window"]["button-three"];
+            UI_body.style.background = "blue";
+            state = "Window";
+            break;
+        case "Outside": UI_text.innerHTML = story["Outside"];
+            button1.innerHTML = storyChoice["Outside"]["button-one"];
+            button2.innerHTML = storyChoice["Outside"]["button-two"];
+            button3.innerHTML = storyChoice["Outside"]["button-three"];
+            UI_body.style.background = "green";
+            break;
         default:
     }
 
@@ -45,7 +67,10 @@ function buttonOneClick() {
     switch (state) {
         case "Home": state = "Window";
             break;
-        case "Window": state = "Window-view"
+        case "Window": state = "Window-view";
+            break;
+        case "Outside": state = "Work";
+            break;
         default:
     }
 
@@ -54,6 +79,18 @@ function buttonOneClick() {
 }
 
 function buttonTwoClick() {
+
+    switch (state) {
+        case "Home": state = "Home-view";
+            break;
+        case "Window": state = "Window-meditate";
+            break;
+        case "Outside": state = "Park";
+            break;
+        default:
+    }
+
+    main();
 
 }
 
@@ -67,6 +104,7 @@ function buttonThreeClick() {
             break;
         case "Window-view": state = "Home";
             break;
+        case "Outside": state = "Home";
         default:
     }
 
