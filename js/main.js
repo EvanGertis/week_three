8c4eb68f54a4b7019b31d75480ad48af30b68f32
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-02-01 13:53:52 -0500

    completely refactored the json object list

--- js/main.js
+++ js/main.js
@@ -20,21 +20,21 @@ let drinks = 12;
 
 // Button states. Each state has three button options. These are used to travese the story tree.
 storyChoice = { 
-    "Home": { "button-one": "view window.", "button-two": "view room.", "button-three": "go outside" }, 
-    "Window": { "button-one": "look out window", "button-two": "meditate", "button-three": "return to room" }, 
-    "Outside": { "button-one": "go to work.", "button-two": "go to the park.", "button-three": "return home."}, 
-    "Work": { "button-one": "kill your boss.", "button-two": "drink coffee and get to work.", "button-three": "return home."}, 
-    "Park": { "button-one": "start a conversation with a stranger.", "button-two": "go to work.", "button-three": "return home."}, 
-    "Kill": {"button-one": "clean up the blood.", "button-two": "confess to what you've done.", "button-three": "drink coffee."},  
-    "Busted": {"button-one": "play again?", "button-two": "go to jail", "button-three": "----"},
-    "Jail": {"button-one": "----", "button-two": "----", "button-three": "----"}, 
-    "Conversation": {"button-one": "kill them.", "button-two": "go on date.", "button-three": "go home."},
-    "Date": {"button-one": "Kill your date.", "button-two": "Go dancing.", "button-three": "Go home."},
-    "Dancing": {"button-one": "Get drunk.", "button-two": "Get high.", "button-three": "Go home."},
-    "Drugs": {"button-one": "Do more drugs.", "button-two": "Get drunk.", "button-three": "Go home."},
-    "Drunk": {"button-one": "''I'm okay!'', keep drinking.", "button-two": "Do some drugs.", "button-three": "Go home."},
-    "Busted": {"button-one": "play again?", "button-two": "go to jail.", "button-three": "----"},
-    "Death": {"button-one": "play again?", "button-two": "-----", "button-three": "----"}
+    home: { b1: "view window.", b2: "view room.",b3: "go outside" }, 
+    window: { b1: "look out window", b2: "meditate", b3: "return to room" }, 
+    outside: { b1: "go to work.", b2: "go to the park.", b3: "return home."}, 
+    work: { b1: "kill your boss.", b2: "drink coffee and get to work.", b3: "return home."}, 
+    park: { b1: "start a conversation with a stranger.", b2: "go to work.", b3: "return home."}, 
+    kill: {b1: "clean up the blood.", b2: "confess to what you've done.", b3: "drink coffee."},  
+    busted: {b1: "play again?", b2: "go to jail", b3: "----"},
+    jail: {b1: "----", b2: "----", b3: "----"}, 
+    conversation: {b1: "kill them.", b2: "go on date.", b3: "go home."},
+    date: {b1: "Kill your date.", b2: "Go dancing.", b3: "Go home."},
+    dancing: {b1: "Get drunk.", b2: "Get high.", b3: "Go home."},
+    drugs: {b1: "Do more drugs.", b2: "Get drunk.", b3: "Go home."},
+    drunk: {b1: "''I'm okay!'', keep drinking.", b2: "Do some drugs.", b3: "Go home."},
+    busted: {b1: "play again?", b2: "go to jail.", b3: "----"},
+    death: {b1: "play again?", b2: "-----", b3: "----"}
     };
 
 main();
@@ -45,135 +45,136 @@ function main() {
     // Story states. This is a JSON where the key is the story state and the value is the paragraph that is displayed in the UI.
     // Needs to be in main to update the clicks in the jail state. 
     story = { 
-        "Home": "You live in a modest house with Ikea furniture and cream colored walls", 
-        "Home-view": "You have done well for yourself, but this place seems rather empty", 
-        "Window": "You are infront of a large bay window.", "Window-view": "What a lovely day it is.", 
-        "Window-meditate":"Ah and so we continue along the path of enlightenment.", 
-        "Outside": "What a lovely day. Perhaps, too nice to even go to work?", "Work":"Another day of hard labor, little pay, and medocrity.", 
-        "Park": "Ah the park. Sometimes you just got to get outside. I wish I could share this day with someone else.", 
-        "Kill": "Violent eh? YOU PSYCOPATH! THERE IS SO MUCH BLOOD!", 
-        "Coffee": "Buona coffee, not too bad I should get some for home.",
-        "Busted": "great. now you've done it you're so busted.", "Jail":`You're in jail. You have no more options. Enjoy! clicks remaining: ${clicks}`, 
-        "Conversation":"Beautiful day isn't it? <br> Stranger: Almost as beautiful as you.",
-        "Date":"It is nice to spend time with someone other than myself. <br> Stranger: Yeah, it sure is. <br> I feel like I am stuck in a circle. I don't know how to get out. <br> Stranger: Life is just a recollection of a past that has yet present itself. <br> This seems like deja vu. ",
-        "Dancing": "You are in a crowded nightclub with loud music",
-        "Drunk": `You puke all over your date: Alcohol tolerance: ${drinks}`,
-        "Drugs":`You are high on drugs. Hits left until overdose: ${hits} `,
-        "Death":`You're wasted. Hope you enjoyed life.`, 
+        home: "You live in a modest house with Ikea furniture and cream colored walls", 
+        home_view: "You have done well for yourself, but this place seems rather empty", 
+        window: "You are infront of a large bay window.", 
+        window_view: "What a lovely day it is.", 
+        window_meditate:"Ah and so we continue along the path of enlightenment.", 
+        outside: "What a lovely day. Perhaps, too nice to even go to work?", "Work":"Another day of hard labor, little pay, and medocrity.", 
+        park: "Ah the park. Sometimes you just got to get outside. I wish I could share this day with someone else.", 
+        kill: "Violent eh? YOU PSYCOPATH! THERE IS SO MUCH BLOOD!", 
+        coffee: "Buona coffee, not too bad I should get some for home.",
+        busted: "great. now you've done it you're so busted.", "Jail":`You're in jail. You have no more options. Enjoy! clicks remaining: ${clicks}`, 
+        conversation:"Beautiful day isn't it? <br> Stranger: Almost as beautiful as you.",
+        date:"It is nice to spend time with someone other than myself. <br> Stranger: Yeah, it sure is. <br> I feel like I am stuck in a circle. I don't know how to get out. <br> Stranger: Life is just a recollection of a past that has yet present itself. <br> This seems like deja vu. ",
+        dancing: "You are in a crowded nightclub with loud music",
+        drunk: `You puke all over your date: Alcohol tolerance: ${drinks}`,
+        drugs:`You are high on drugs. Hits left until overdose: ${hits} `,
+        death:`You're wasted. Hope you enjoyed life.`, 
         };
 
     switch (state) {
-        case "Home": UI_text.innerHTML = story["Home"];
-            button1.innerHTML = storyChoice["Home"]["button-one"];
-            button2.innerHTML = storyChoice["Home"]["button-two"];
-            button3.innerHTML = storyChoice["Home"]["button-three"];
+        case "Home": UI_text.innerHTML = story.home;
+            button1.innerHTML = storyChoice.home.b1;
+            button2.innerHTML = storyChoice.home.b2;
+            button3.innerHTML = storyChoice.home.b3;
             UI_body.style.background = "pink";
             break;
-        case "Home-view": UI_text.innerHTML = story["Home-view"];
-            button1.innerHTML = storyChoice["Home"]["button-one"];
-            button2.innerHTML = storyChoice["Home"]["button-two"];
-            button3.innerHTML = storyChoice["Home"]["button-three"];
+        case "Home-view": UI_text.innerHTML = story.home_view;
+            button1.innerHTML = storyChoice.home.b1;
+            button2.innerHTML = storyChoice.home.b2;
+            button3.innerHTML = storyChoice.home.b3;
             UI_body.style.background = "pink";
             state = "Home";
             break;
-        case "Window": UI_text.innerHTML = story["Window"];
-            button1.innerHTML = storyChoice["Window"]["button-one"];
-            button2.innerHTML = storyChoice["Window"]["button-two"];
-            button3.innerHTML = storyChoice["Window"]["button-three"];
+        case "Window": UI_text.innerHTML = story.window;
+            button1.innerHTML = storyChoice.window.b1;
+            button2.innerHTML = storyChoice.window.b2;
+            button3.innerHTML = storyChoice.window.b3;
             UI_body.style.background = "blue";
             break;
-        case "Window-view": UI_text.innerHTML = story["Window-view"];
-            button1.innerHTML = storyChoice["Window"]["button-one"];
-            button2.innerHTML = storyChoice["Window"]["button-two"];
-            button3.innerHTML = storyChoice["Window"]["button-three"];
-            UI_body.style.background = "blue";
-            state = "Window";
-            break;
-        case "Window-meditate": UI_text.innerHTML = story["Window-meditate"];
-            button1.innerHTML = storyChoice["Window"]["button-one"];
-            button2.innerHTML = storyChoice["Window"]["button-two"];
-            button3.innerHTML = storyChoice["Window"]["button-three"];
+        case "Window-view": UI_text.innerHTML = story.window_view;
+            button1.innerHTML = storyChoice.window.b1;
+            button2.innerHTML = storyChoice.window.b2;
+            button3.innerHTML = storyChoice.window.b3;
             UI_body.style.background = "blue";
             state = "Window";
             break;
-        case "Outside": UI_text.innerHTML = story["Outside"];
-            button1.innerHTML = storyChoice["Outside"]["button-one"];
-            button2.innerHTML = storyChoice["Outside"]["button-two"];
-            button3.innerHTML = storyChoice["Outside"]["button-three"];
+        case "Window-meditate": UI_text.innerHTML = story.window_meditate;
+            button1.innerHTML = storyChoice.window.b1;
+            button2.innerHTML = storyChoice.window.b2;
+            button3.innerHTML = storyChoice.window.b3;
+            UI_body.style.background = "blue";
+            state = "Window";
+            break;
+        case "Outside": UI_text.innerHTML = story.outside;
+            button1.innerHTML = storyChoice.outside.b1;
+            button2.innerHTML = storyChoice.outside.b2;
+            button3.innerHTML = storyChoice.outside.b3;
             UI_body.style.background = "green";
             break;
-        case "Work": UI_text.innerHTML = story["Work"];
-            button1.innerHTML = storyChoice["Work"]["button-one"];
-            button2.innerHTML = storyChoice["Work"]["button-two"];
-            button3.innerHTML = storyChoice["Work"]["button-three"];
+        case "Work": UI_text.innerHTML = story.work;
+            button1.innerHTML = storyChoice.work.b1;
+            button2.innerHTML = storyChoice.work.b2;
+            button3.innerHTML = storyChoice.work.b3;
             UI_body.style.background = "black";
             break;
-        case "Park": UI_text.innerHTML = story["Park"];
-            button1.innerHTML = storyChoice["Park"]["button-one"];
-            button2.innerHTML = storyChoice["Park"]["button-two"];
-            button3.innerHTML = storyChoice["Park"]["button-three"];
+        case "Park": UI_text.innerHTML = story.park;
+            button1.innerHTML = storyChoice.park.b1;
+            button2.innerHTML = storyChoice.park.b2;
+            button3.innerHTML = storyChoice.park.b3;
             UI_body.style.background = "aquamarine";
             break;
-        case "Kill": UI_text.innerHTML = story["Kill"];
-            button1.innerHTML = storyChoice["Kill"]["button-one"];
-            button2.innerHTML = storyChoice["Kill"]["button-two"];
-            button3.innerHTML = storyChoice["Kill"]["button-three"];
+        case "Kill": UI_text.innerHTML = story.kill;
+            button1.innerHTML = storyChoice.kill.b1;
+            button2.innerHTML = storyChoice.kill.b2;
+            button3.innerHTML = storyChoice.kill.b3;
             UI_body.style.backgroundImage = "url('https://thumbs.gfycat.com/BlondNiftyHochstettersfrog-max-1mb.gif')";
             break;
-        case "Coffee": UI_text.innerHTML = story["Coffee"];
-            button1.innerHTML = storyChoice["Work"]["button-one"];
-            button2.innerHTML = storyChoice["Work"]["button-two"];
-            button3.innerHTML = storyChoice["Work"]["button-three"];
+        case "Coffee": UI_text.innerHTML = story.coffee;
+            button1.innerHTML = storyChoice.work.b1;
+            button2.innerHTML = storyChoice.work.b2;
+            button3.innerHTML = storyChoice.work.b3;
             UI_body.style.background = "brown";
             state = "Work";
             break;
-        case "Busted": UI_text.innerHTML = story["Busted"];
-            button1.innerHTML = storyChoice["Busted"]["button-one"];
-            button2.innerHTML = storyChoice["Busted"]["button-two"];
-            button3.innerHTML = storyChoice["Busted"]["button-three"];
+        case "Busted": UI_text.innerHTML = story.busted;
+            button1.innerHTML = storyChoice.busted.b1;
+            button2.innerHTML = storyChoice.busted.b2;
+            button3.innerHTML = storyChoice.busted.b3;
             UI_body.style.backgroundImage = "url('https://www.lequzhai.com/data/out/12/497132.gif')";
             break;
-        case "Jail": UI_text.innerHTML = story["Jail"];
-            button1.innerHTML = storyChoice["Jail"]["button-one"];
-            button2.innerHTML = storyChoice["Jail"]["button-two"];
-            button3.innerHTML = storyChoice["Jail"]["button-three"];
+        case "Jail": UI_text.innerHTML = story.jail;
+            button1.innerHTML = storyChoice.jail.b1;
+            button2.innerHTML = storyChoice.jail.b2;
+            button3.innerHTML = storyChoice.jail.b3;
             UI_body.style.background = "gray";
             break;
-        case "Conversation": UI_text.innerHTML = story["Conversation"];
-            button1.innerHTML = storyChoice["Conversation"]["button-one"];
-            button2.innerHTML = storyChoice["Conversation"]["button-two"];
-            button3.innerHTML = storyChoice["Conversation"]["button-three"];
+        case "Conversation": UI_text.innerHTML = story.conversation;
+            button1.innerHTML = storyChoice.conversation.b1;
+            button2.innerHTML = storyChoice.conversation.b2;
+            button3.innerHTML = storyChoice.conversation.b3;
             UI_body.style.background = "purple";
             break;
-        case "Date": UI_text.innerHTML = story["Date"];
-            button1.innerHTML = storyChoice["Date"]["button-one"];
-            button2.innerHTML = storyChoice["Date"]["button-two"];
-            button3.innerHTML = storyChoice["Date"]["button-three"];
+        case "Date": UI_text.innerHTML = story.date;
+            button1.innerHTML = storyChoice.date.b1;
+            button2.innerHTML = storyChoice.date.b2;
+            button3.innerHTML = storyChoice.date.b3;
             UI_body.style.background = "orange";
             break;
-        case "Drunk": UI_text.innerHTML = story["Drunk"];
-            button1.innerHTML = storyChoice["Drunk"]["button-one"];
-            button2.innerHTML = storyChoice["Drunk"]["button-two"];
-            button3.innerHTML = storyChoice["Drunk"]["button-three"];
+        case "Drunk": UI_text.innerHTML = story.drunk;
+            button1.innerHTML = storyChoice.drunk.b1;
+            button2.innerHTML = storyChoice.drunk.b2;
+            button3.innerHTML = storyChoice.drunk.b3;
             UI_body.style.backgroundImage = "url('https://i.gifer.com/IbdT.gif')";
             state = "Drunk";
             break;
-        case "Drugs": UI_text.innerHTML = story["Drugs"];
-            button1.innerHTML = storyChoice["Drugs"]["button-one"];
-            button2.innerHTML = storyChoice["Drugs"]["button-two"];
-            button3.innerHTML = storyChoice["Drugs"]["button-three"];
+        case "Drugs": UI_text.innerHTML = story.drugs;
+            button1.innerHTML = storyChoice.drugs.b1;
+            button2.innerHTML = storyChoice.drugs.b2;
+            button3.innerHTML = storyChoice.drugs.b3;
             state = "Drugs";
             break;
-        case "Dancing": UI_text.innerHTML = story["Dancing"];
-            button1.innerHTML = storyChoice["Dancing"]["button-one"];
-            button2.innerHTML = storyChoice["Dancing"]["button-two"];
-            button3.innerHTML = storyChoice["Dancing"]["button-three"];
+        case "Dancing": UI_text.innerHTML = story.dancing;
+            button1.innerHTML = storyChoice.dancing.b1;
+            button2.innerHTML = storyChoice.dancing.b2;
+            button3.innerHTML = storyChoice.dancing.b3;
             UI_body.style.backgroundImage = "url('https://media.giphy.com/media/C3EAt5F9qfHPO/giphy.gif')";
             break;
-        case "Death": UI_text.innerHTML = story["Death"];
-            button1.innerHTML = storyChoice["Death"]["button-one"];
-            button2.innerHTML = storyChoice["Death"]["button-two"];
-            button3.innerHTML = storyChoice["Death"]["button-three"];
+        case "Death": UI_text.innerHTML = story.death;
+            button1.innerHTML = storyChoice.death.b1;
+            button2.innerHTML = storyChoice.death.b2;
+            button3.innerHTML = storyChoice.death.b3;
             UI_body.style.backgroundImage = "url('https://media.giphy.com/media/YwAgyCddum3K0/giphy.gif')";
             break;
         default:
